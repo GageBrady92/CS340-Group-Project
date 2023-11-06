a9044497ab5d385e1e30aab301ce03beb073c2ff
Daniel Brady <bradyda@flip2.engr.oregonstate.edu>
2023-11-06 12:04:09 -0800

    Update for create

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
-/*
-    SETUP
-*/
-
-// Express
 var express = require('express');
 var app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
-PORT = 10168;
-
+ 
+PORT = 10167;
+ 
 // Database
 var db = require('./database/db-connector');
-
+ 
 // Handlebars
 var exphbs = require('express-handlebars');
 const { query } = require('express');
-app.engine('.hbs', exphbs({
+app.engine('.hbs', exphbs.engine({
     extname: ".hbs"
 }));
 app.set('view engine', '.hbs');
-
+ 
 // Static Files
 app.use(express.static('public'));
-
-
 
 /*
     ROUTES
 */
-    
 // app.js
 
 app.get('/', function(req, res)
@@ -40,10 +32,60 @@ app.get('/', function(req, res)
 
             res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
-    });                                                         // received back from the query                                        // will process this file, before sending the finished HTML to the client.
 
-
-
+    });
+    
+    app.post('/add-restaurant-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+    
+        // // Capture NULL values
+        // let homeworld = parseInt(data.homeworld);
+        // if (isNaN(homeworld))
+        // {
+        //     homeworld = 'NULL'
+        // }
+    
+        // let age = parseInt(data.age);
+        // if (isNaN(age))
+        // {
+        //     age = 'NULL'
+        // }
+    
+        // Create the query and run it on the database
+        query1 = `INSERT INTO Restaurants (location, food_type) VALUES ('${data.location}', '${data.food_type}')`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on bsg_people
+                query2 = `SELECT * FROM Restaurants;`;
+                db.pool.query(query2, function(error, rows, fields){
+    
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        res.send(rows);
+                    }
+                })
+            }
+        })
+    });
     app.post('/add-location-form', function(req, res){
         // Capture the incoming data and parse it back to a JS object
         let data = req.body;
@@ -82,9 +124,9 @@ app.get('/', function(req, res)
         })
     })
     
-    /*
-        LISTENER
-    */
-    app.listen(PORT, function(){
-        console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
-    });
+/*
+    LISTENER
+*/
+app.listen(PORT, function(){
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
