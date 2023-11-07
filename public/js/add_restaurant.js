eef5f56c0c5944507803b24d33b7f9240d3f175a
Aubrey Floyd <floydau@flip2.engr.oregonstate.edu>
2023-11-06 19:19:15 -0800

    add restaurant javascript with updated delete commented out

--- public/js/add_restaurant.js
+++ public/js/add_restaurant.js
@@ -48,12 +48,12 @@
 // })
 
 
-// // Creates a single row from an Object representing a single record from 
+// // Creates a single row from an Object representing a single record from
 // // bsg_people
 // addRowToTable = (data) => {
 
 //     // Get a reference to the current table on the page and clear it out.
-//     let currentTable = document.getElementById("restaurant-table");
+//     let currentTable = document.getElementById("people-table");
 
 //     // Get the location where we should insert the new row (end of table)
 //     let newRowIndex = currentTable.rows.length;
@@ -62,22 +62,41 @@
 //     let parsedData = JSON.parse(data);
 //     let newRow = parsedData[parsedData.length - 1]
 
-//     // Create a row and 2 cells
+//     // Create a row and 4 cells
 //     let row = document.createElement("TR");
-//     let restaurant_idCell = document.createElement("TD");
-//     let locationCell = document.createElement("TD");
-//     let foodTypeCell = document.createElement("TD");
+//     let idCell = document.createElement("TD");
+//     let firstNameCell = document.createElement("TD");
+//     let lastNameCell = document.createElement("TD");
+//     let homeworldCell = document.createElement("TD");
+//     let ageCell = document.createElement("TD");
+
+//     let deleteCell = document.createElement("TD");
 
 //     // Fill the cells with correct data
-//     restaurant_idCell.innerText = newRow.id;
-//     locationCell.innerText = newRow.location;
-//     foodTypeCell.innerText = newRow.food_type;
+//     idCell.innerText = newRow.id;
+//     firstNameCell.innerText = newRow.fname;
+//     lastNameCell.innerText = newRow.lname;
+//     homeworldCell.innerText = newRow.homeworld;
+//     ageCell.innerText = newRow.age;
 
-//     // Add the cells to the row 
-//     row.appendChild(restaurant_idCell);
-//     row.appendChild(locationCell);
-//     row.appendChild(foodTypeCell);
-    
+//     deleteCell = document.createElement("button");
+//     deleteCell.innerHTML = "Delete";
+//     deleteCell.onclick = function(){
+//         deletePerson(newRow.id);
+//     };
+
+
+//     // Add the cells to the row
+//     row.appendChild(idCell);
+//     row.appendChild(firstNameCell);
+//     row.appendChild(lastNameCell);
+//     row.appendChild(homeworldCell);
+//     row.appendChild(ageCell);
+//     row.appendChild(deleteCell);
+
+//     // Add a row attribute so the deleteRow function can find a newly added row
+//     row.setAttribute('data-value', newRow.id);
+
 //     // Add the row to the table
 //     currentTable.appendChild(row);
 // }
